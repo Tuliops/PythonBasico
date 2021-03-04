604cb665253c3ad4744a9dacbe19cecbe93314c5
Tulio <TulioPirir3@gmail.com>
2021-03-03 18:09:46 -0600

    Hoja De Trabajo 1
    HT1

---
+++ HojaDeTrabajo1.py
@@ -0,0 +1,13 @@
+print("---Hoja de Trabajo 1")
+print()
+print("Ingrese Su Peso (kg)")
+peso= float(input())
+print()
+print ("Engrese Su Estatura (m)")
+estatura =float(input())
+imc=float(0);
+imc=peso/estatura**2
+print("Su Indice de Masa Corporal es de ","{0:.2f}".format(imc))
+print()
+print()
+
